1452d424b2c675828000c71deda77e5e204bb7fc
AsynchronouZ <45092088+AsynchronouZ@users.noreply.github.com>
2021-06-04 15:48:17 +0800

    Add files via upload

---
+++ SchoolRegistration/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <link rel="stylesheet" href="./static/css/utilities.css">
+   <link rel="stylesheet" type="text/css" href="static/css/homepage.css">
+   <link rel="icon" href="BCP.png">
+   <title>VSchool Where Great Programmers Comes from</title>
+</head>
+
+<body>
+   <section class="greeting-sec">
+      <nav>
+         <a href="#" class="logo">Tbis102</a>
+         <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="./static/php/login.php">Admin</a></li>
+         </ul>
+      </nav>
+      <h2 id="text">VSchool</h2>
+      <h2 id="text2">An Online Virtual School for Every aspiring Programmers</h2>
+      <a href="./static/php/Student_Registration.php" id="btn">Register</a>
+      <img src="static/assets/img/stars.png" id="stars">
+      <img src="static/assets/img/moon.png" id="moon">
+      <img src="static/assets/img/mountains_behind.png" id="mountains_behind">
+      <img src="static/assets/img/mountains_front.png" id="mountains_front">
+   </section>
+
+   <section>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+   </section>
+
+
+   <script src="./static/js/index.js"></script>
+   <script src="./static/js/homepage.js"></script>
+   <script>
+   homepageAnims();
+   </script>
+</body>
+
+</html>
